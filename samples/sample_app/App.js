f13e12d350cc8bcbb0b3df802466b4630c2fc7f0
Saranga Rathnayaka <sarangar@99x.lk>
2018-10-06 04:57:50 +0530

    #2 Display sample website using library in android

--- samples/sample_app/App.js
+++ samples/sample_app/App.js
@@ -1,58 +1,25 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- * @flow
- */
-
 import React, { Component } from 'react';
-import {
-  Platform,
-  StyleSheet,
-  Text,
-  View
-} from 'react-native';
+import { Text, View, Dimensions } from 'react-native';
 
-const instructions = Platform.select({
-  ios: 'Press Cmd+R to reload,\n' +
-    'Cmd+D or shake for dev menu',
-  android: 'Double tap R on your keyboard to reload,\n' +
-    'Shake or press menu button for dev menu',
-});
+const {height,width} = Dimensions.get('window');
 
-type Props = {};
-export default class App extends Component<Props> {
+import AdvancedWebView from 'advanced-react-native-webview';
+
+export default class App extends Component {
+
+  constructor(){
+    super();
+    this.src = 'https://www.99xtechnology.com/';
+    console.log('test');
+  }
   render() {
     return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit App.js
-        </Text>
-        <Text style={styles.instructions}>
-          {instructions}
-        </Text>
-      </View>
+      <View style={{ flex: 1,justifyContent: 'center',alignItems: 'center' }} >            
+         <AdvancedWebView
+             source={{ uri: this.src }}
+             style={{ flex: 1,width:width,height:height}}
+         />
+       </View>
     );
   }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
-});
